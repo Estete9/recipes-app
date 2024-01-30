d0cc3148ea1403d79e153a0ffc88a38b1f26243b
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-01-30 11:38:57 +0430

    changes the database to postgres

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem "rails", "~> 7.1.2"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
+gem 'pg'
 
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_29_050354) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +25,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_050354) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
