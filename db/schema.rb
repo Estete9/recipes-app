265c2fe7ac897c1a6d293a7b0800cecaa1b1b814
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-01-30 11:28:11 +0430

    fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -22,5 +22,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_050354) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
