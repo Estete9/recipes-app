ecabd3ecc5b371a4eafe02b7ff074b00506b2993
Esteban <cepalacios@outlook.com>
2024-01-30 11:10:16 -0500

    Implmement inventory scaffold with has_many user relatiton

---
+++ spec/views/inventories/edit.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "inventories/edit", type: :view do
+  let(:inventory) {
+    Inventory.create!(
+      name: "MyString",
+      user: nil
+    )
+  }
+
+  before(:each) do
+    assign(:inventory, inventory)
+  end
+
+  it "renders the edit inventory form" do
+    render
+
+    assert_select "form[action=?][method=?]", inventory_path(inventory), "post" do
+
+      assert_select "input[name=?]", "inventory[name]"
+
+      assert_select "input[name=?]", "inventory[user_id]"
+    end
+  end
+end
---
+++ spec/views/inventories/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "inventories/new", type: :view do
+  before(:each) do
+    assign(:inventory, Inventory.new(
+      name: "MyString",
+      user: nil
+    ))
+  end
+
+  it "renders new inventory form" do
+    render
+
+    assert_select "form[action=?][method=?]", inventories_path, "post" do
+
+      assert_select "input[name=?]", "inventory[name]"
+
+      assert_select "input[name=?]", "inventory[user_id]"
+    end
+  end
+end
