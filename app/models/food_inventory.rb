2be945eb6eb5b363576c9e5ea97a0da2ea6ac39d
Esteban <cepalacios@outlook.com>
2024-01-31 11:17:36 -0500

    Implement Inventories details page
    Implement food_inventories table, controller, and model
    Implement inventories#show view
    Implement add food item button to inventories#show
    Implement remove food item button in inventories#show

--- app/models/food_inventory.rb
+++ app/models/food_inventory.rb
@@ -5,4 +5,8 @@ class FoodInventory < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :inventory_id, presence: true
   validates :food_id, presence: true
+
+  def quantity_with_measurement
+    "#{quantity} #{food.measurement_unit}"
+  end
 end
