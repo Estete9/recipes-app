6869f3db39167ce0c42613a57127253c03fdddaa
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-31 21:09:35 +0300

    Remove edit on the before action params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[show update destroy]
   before_action :authenticate_user!
 
   # GET /recipes or /recipes.json
