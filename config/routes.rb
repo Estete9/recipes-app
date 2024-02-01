c9a494ab5cf7a00c0f08609a720bcca6b94a5fe3
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-01 21:02:07 +0300

    Create routes to newly created food recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :food_recipes, only: [:new, :create, :destroy]
   resources :food_inventories, only: [:new, :create, :destroy]
   resources :public_recipes, only: [:index]
   resources :inventories, except: [:edit, :update]
