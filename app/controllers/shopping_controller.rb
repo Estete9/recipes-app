f36bbee706a69195b53858dd7b43a6dbf8e60187
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-02 16:04:13 +0430

    shopping list view controller and route configed

---
+++ app/controllers/shopping_controller.rb
@@ -0,0 +1,11 @@
+class ShoppingController < ApplicationController
+  def generate_shopping_list
+    inventory_id = params[:user_id]
+    recipe_id = params[:recipe_id]
+
+    # You can use inventory_id and recipe_id as needed in your view or business logic
+
+    # Render the view for the shopping list
+    render 'generate_shopping_list'
+  end
+end
