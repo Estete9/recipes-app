12d8511fca5f882d3884a95476ae580c5cb9c31c
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-02 16:43:46 +0430

    inventory named showed in page

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -2,7 +2,7 @@ class ShoppingController < ApplicationController
   def generate_shopping_list
     inventory_id = params[:user_id]
     recipe_id = params[:recipe_id]
-
+@inventory = Inventory.find(inventory_id)
     # You can use inventory_id and recipe_id as needed in your view or business logic
 
     # Render the view for the shopping list
