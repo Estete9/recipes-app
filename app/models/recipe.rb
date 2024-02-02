6bfe5277917ec5892da57114ee18e138fac58612
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-02 15:17:15 +0430

    public recipe completed and many to many association between food and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :food_recipes
+  has_many :foods, through: :food_recipes
+  def total_price
+    total = 0
+    food_recipes.each do |rf|
+      total += rf.food.price
+    end
+    total
+  end
 end
