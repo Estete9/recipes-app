f505fe58a94d2d4a89d54500920f36bbf78467ea
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-02 20:45:56 +0300

    Add validation to recipe model to have a name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :food_recipes
   has_many :foods, through: :food_recipes
+
+  validates :name, presence: true
   def total_price
     total = 0
     food_recipes.each do |rf|
