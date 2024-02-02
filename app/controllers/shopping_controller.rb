87cb0b4373c76839b17413e317f1d81cf73c7ef0
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-02 18:30:00 +0430

    final version ready

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,18 +1,20 @@
 class ShoppingController < ApplicationController
-# app/controllers/shopping_controller.rb
-def generate_shopping_list
-  inventory_id = params[:user_id]
-  recipe_id = params[:recipe_id]
-  @inventory = Inventory.find(inventory_id)
-  @recipe = Recipe.find_by(id: recipe_id)
+  def generate_shopping_list
+    inventory_id = params[:user_id]
+    recipe_id = params[:recipe_id]
 
+    @inventory = Inventory.find(inventory_id)
+    @recipe = Recipe.find_by(id: recipe_id)
 
+    # You can use inventory_id and recipe_id as needed in your view or business logic
+    @foods = @inventory.foods
+    @food_of_recipe = @recipe.foods
+    @food_of_inventory = @inventory.foods
 
-  # You can use inventory_id and recipe_id as needed in your view or business logic
-  @foods = @inventory.foods
+    # Find the missing foods for the recipe
+    @missing_foods = @food_of_recipe - @foods
 
-  # Render the view for the shopping list
-  render 'generate_shopping_list'
-end
-
+    # Render the view for the shopping list
+    render 'generate_shopping_list'
+  end
 end
