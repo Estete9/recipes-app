e65d8d4546ad46b64386657082a46164071880a9
Esteban <cepalacios@outlook.com>
2024-02-02 17:33:35 -0500

    Hotfix: fix recipe table so it displays quantity

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
-  def name_with_measurement_unit
-    "#{name} (#{measurement_unit})"
-  end
   has_many :food_recipes, dependent: :destroy
   has_many :recipes, through: :food_recipes
+
   def total_price
     total = 0
     food_recipes.each do |rf|
--- app/models/food_recipe.rb
+++ app/models/food_recipe.rb
@@ -1,4 +1,8 @@
 class FoodRecipe < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
+
+   def quantity_with_measurement_unit
+    "#{quantity} (#{food.measurement_unit})"
+  end
 end
