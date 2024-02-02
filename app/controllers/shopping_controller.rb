43626e4495b9af2f55f4058881db1664c867674d
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-02 16:51:32 +0430

    view for shopping imporved further

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -4,7 +4,7 @@ class ShoppingController < ApplicationController
     recipe_id = params[:recipe_id]
 @inventory = Inventory.find(inventory_id)
     # You can use inventory_id and recipe_id as needed in your view or business logic
-
+@foods = @inventory.foods
     # Render the view for the shopping list
     render 'generate_shopping_list'
   end
